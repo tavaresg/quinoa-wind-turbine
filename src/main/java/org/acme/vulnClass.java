8107f76936f7bde9936d620cba094495fde58208
Gabriel Tavares <gtavares@redhat.com>
2023-07-14 19:28:01 -0300

    vulnClass

--- src/main/java/org/acme/vulnClass.java
+++ src/main/java/org/acme/vulnClass.java
@@ -1,17 +1,23 @@
 package org.acme;
 
 import java.sql.ResultSet;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipEntry;
 
   public class vulnClass {
-  // Get username from parameters
-  String username = request.getParameter("username");
-  // Create a statement from database connection
-  Statement statement = connection.createStatement();  
-  // Create unsafe query by concatenating user defined data with query string
-  String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
-  // ... OR ...
-  // Insecurely format the query string using user defined data 
-  String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
-  // Execute query and return the results
-  ResultSet result = statement.executeQuery(query);
+
+
+    public void extract(ZipFile zip) {
+        String toDir = "/my/target/directory/";
+        Enumeration entries = zip.entries();
+        
+        while (entries.hasMoreElements()) {
+            ZipEntry zipEntry = entries.nextElement();
+            File file = new File(toDir, zipEntry.getName())
+            InputStream istr = zipFile.getInputStream(zipEntry);
+            final OutputStream os = Files.newOutputStream(file.toPath());
+            bos  = new BufferedOutputStream(os);
+            IOUtils.copy(bis, bos);
+        }
+    }
 }
